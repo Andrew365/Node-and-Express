cf04e0652b6a5c5afd59da5b346fb0f5a0587b7a
AWboy <baseball_allstar_365@hotmail.com>
2015-07-19 21:49:15 -0600

    added other views and tried to start passport

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var login = require('./routes/login');
+var reg = require('./routes/register');
 
 var app = express();
 
@@ -23,7 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/login', login);
+app.use('/register', reg);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
