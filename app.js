ff2eb5a0f5829a8381cedfff493b67cd54292e96
AWboy <baseball_allstar_365@hotmail.com>
2015-07-20 19:03:05 -0600

    added help stuff and installed yahoo-finance

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var passport = require('passport');
 var routes = require('./routes/index');
 var login = require('./routes/login');
 var reg = require('./routes/register');
+var help = require('./routes/help');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/login', login);
 app.use('/register', reg);
+app.use('/help', help);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
